9138c65a6a7e0a2ea2c9f8c62abfb6bf865925d3
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-10-24 17:10:40 +0700

    initiate new project

---
+++ macro-team15/macro_team15App.swift
@@ -0,0 +1,17 @@
+//
+//  macro_team15App.swift
+//  macro-team15
+//
+//  Created by Aditya Cahyo on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct macro_team15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
