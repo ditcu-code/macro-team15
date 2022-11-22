b04d0e53b46086c06973260d421264312fcb0d1f
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-11-22 15:36:40 +0700

    Highlighted stimulus cta

--- macro-team15/View/Dashboard/HighlightedStimulusView.swift
+++ macro-team15/View/Dashboard/HighlightedStimulusView.swift
@@ -9,31 +9,48 @@ import SwiftUI
 
 struct HighlightedStimulusView: View {
     var body: some View {
-        Image("PlaceholderImage")
-            .resizable()
-        
         VStack {
-            Text("Aktivitas")
-                .font(.callout)
-                .fontWeight(.bold)
+            Image("PlaceholderImage")
+                .resizable()
             
-            Text("Tummy Time")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.vertical, 4)
+            VStack {
+                Text("Aktivitas")
+                    .font(.callout)
+                    .fontWeight(.bold)
+                
+                Text("Tummy Time")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.vertical, 4)
+                
+                Text("Aktivitas ini dapat mendukung pencapaian motorik dan kognitif!")
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.white)
+                    .shadow(radius: 4)
+            )
+            .padding()
+            .offset(CGSize(width: 0, height: -80))
+            .padding(.bottom, -100)
             
-            Text("Aktivitas ini dapat mendukung pencapaian motorik dan kognitif!")
-                .multilineTextAlignment(.center)
+            Button {
+                
+            } label: {
+                Text("Lakukan")
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(Color.ui.primary)
+                    )
+                    .padding()
+            }
         }
-        .padding()
-        .background(
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(.white)
-                .shadow(radius: 4)
-        )
-        .padding()
-        .offset(CGSize(width: 0, height: -80))
-        .padding(.bottom, -80)
     }
 }
 
