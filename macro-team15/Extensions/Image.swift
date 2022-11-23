4bdc14020b16e515400787045de6cfa99b1f571d
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-11-23 16:58:11 +0700

    Mission view

---
+++ macro-team15/Extensions/Image.swift
@@ -0,0 +1,18 @@
+//
+//  Image.swift
+//  macro-team15
+//
+//  Created by Darma Wiryanata on 23/11/22.
+//
+
+import SwiftUI
+
+extension Image {
+
+    static let ui = Image.UI()
+
+    struct UI {
+        let placeholder = Image("PlaceholderImage")
+    }
+
+}
