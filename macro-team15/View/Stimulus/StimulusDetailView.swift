511adeb2775214a9497c92b6c87953176d58d322
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-11-26 16:36:50 +0700

    Screen background

--- macro-team15/View/Stimulus/StimulusDetailView.swift
+++ macro-team15/View/Stimulus/StimulusDetailView.swift
@@ -9,50 +9,55 @@ import SwiftUI
 
 struct StimulusDetailView: View {
     var body: some View {
-        ScrollView {
-            HighlightedStimulusView(withCTA: false)
+        ZStack {
+            Color.ui.background
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                HighlightedStimulusView(withCTA: false)
 
-            Divider()
-                .padding(.top)
-                .padding(.bottom, 8)
+                Divider()
+                    .padding(.top)
+                    .padding(.bottom, 8)
 
-            ContentHeaderView(title: "Material", subtitle: "Peralatan untuk mendukung aktivitas ini", navigationLink: nil)
+                ContentHeaderView(title: "Material", subtitle: "Peralatan untuk mendukung aktivitas ini", navigationLink: nil)
 
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    Spacer()
-                        .padding(.leading, 10)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        Spacer()
+                            .padding(.leading, 10)
 
-                    ForEach(0 ..< 5) { item in
-                        StimulusMaterialView()
-                            .padding(.trailing, 10)
+                        ForEach(0 ..< 5) { item in
+                            StimulusMaterialView()
+                                .padding(.trailing, 10)
+                        }
                     }
                 }
-            }
-            .padding(.top)
-
-            Divider()
                 .padding(.top)
-                .padding(.bottom, 12)
 
-            ContentHeaderView(title: "Langkah-langkah", subtitle: "Panduan untuk aktivitas ini", navigationLink: nil)
-            
-            ForEach(1 ..< 5) { item in
-                StimulusStepView(order: item, description: "Lakukan Tummy Time di tempat yang datar seperti lantai, kasur, atau di atas perut dan pangkuan bunda")
-            }
-        }
-        
-        .navigationTitle(Text("Detail Aktivitas"))
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "square.and.arrow.up")
-                        .foregroundColor(Color.ui.primary)
+                Divider()
+                    .padding(.top)
+                    .padding(.bottom, 12)
+
+                ContentHeaderView(title: "Langkah-langkah", subtitle: "Panduan untuk aktivitas ini", navigationLink: nil)
+                
+                ForEach(1 ..< 5) { item in
+                    StimulusStepView(order: item, description: "Lakukan Tummy Time di tempat yang datar seperti lantai, kasur, atau di atas perut dan pangkuan bunda")
                 }
             }
+            
+            .navigationTitle(Text("Detail Aktivitas"))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "square.and.arrow.up")
+                            .foregroundColor(Color.ui.primary)
+                    }
+                }
+        }
         }
     }
 }
