ec4dc58cdde3db4b2aeb422a5e811807545d0a9d
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-11-28 01:07:00 +0700

    Revert "Ditcu/hotfix (#30)"
    This reverts commit 88b74c8c6ebf3dfe56e266beb79e4a36e8cef848.

--- macro-team15/Extensions/ButtonStyle.swift
+++ macro-team15/Extensions/ButtonStyle.swift
@@ -8,21 +8,16 @@
 import SwiftUI
 
 struct PrimaryButtonStyle: ButtonStyle {
-    @Environment(\.isEnabled) var isEnabled
-    
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
             .font(.body.bold())
             .foregroundColor(.white)
-            .frame(maxWidth: .infinity, minHeight: 60)
+            .padding()
+            .frame(maxWidth: .infinity)
             .background(
                 RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(isEnabled ? Color.ui.primary : Color.ui.primary.opacity(0.2))
+                    .foregroundColor(Color.ui.primary)
             )
             .scaleEffect(configuration.isPressed ? 0.95: 1)
-            .shadow(
-                color: .gray.opacity(isEnabled ? 0.5 : 0),
-                radius: 2, x: 0, y: 1
-            )
     }
 }
--- macro-team15/View/Dashboard/DashboardScreen.swift
+++ macro-team15/View/Dashboard/DashboardScreen.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct DashboardScreen: View {
     
     let name: String
-//    @FetchRequest(sortDescriptors: [])
-//    private var babyList: FetchedResults<Baby>
-//
+    
     @State private var selectedMilestoneCategory = 1
     
     var body: some View {
@@ -23,7 +21,6 @@ struct DashboardScreen: View {
                         .edgesIgnoringSafeArea(.all)
                     
                     ScrollView {
-
                         HighlightedStimulusView(withCTA: true)
                         
                         Divider()
@@ -70,7 +67,7 @@ struct DashboardScreen: View {
                     }
                 }
                 
-                .navigationTitle("\(geo.frame(in: .global).minY < 100 ? "Beranda" : "Hi, \(name)!")")
+                .navigationTitle("\(geo.frame(in: .global).minY < 100 ? "Beranda" : "Hi, Ceroy!")")
                 .toolbar {
                     // Milestone dropdown
                     ToolbarItem(placement: .navigationBarLeading) {
