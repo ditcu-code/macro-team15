d979b201c62e0d610e628e638f253936af6c5eaa
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-12-02 18:03:01 +0700

    add highlightedstimulusview version 2

--- macro-team15/View/Dashboard/HighlightedStimulusView.swift
+++ macro-team15/View/Dashboard/HighlightedStimulusView.swift
@@ -50,9 +50,54 @@ struct HighlightedStimulusView: View {
         }
     }
 }
+//
+//struct HighlightedStimulusView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HighlightedStimulusView(withCTA: true)
+//    }
+//}
 
-struct HighlightedStimulusView_Previews: PreviewProvider {
-    static var previews: some View {
-        HighlightedStimulusView(withCTA: true)
+struct HighlightedStimulusViewV2: View {
+    
+    let withCTA: Bool
+    let stimulus: Stimulus
+    
+    var body: some View {
+        VStack {
+            Image.ui.tummyTime
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxHeight: 150)
+                .padding(.vertical, 30)
+            
+            VStack {
+                Text(stimulus.type.rawValue)
+                    .font(.custom(FontType.regular.rawValue, size: 16))
+                    .foregroundColor(Color.ui.text)
+                
+                Divider().padding(.horizontal, 60)
+                
+                Text(stimulus.activityName)
+                    .font(.custom(FontType.semiBold.rawValue, size: 30))
+                    .padding(.vertical, 3)
+                    .foregroundColor(Color.ui.primary)
+                
+                Text("Aktivitas ini dapat mendukung pencapaian \(StringManipulation.categoryStimulus(cat: stimulus.categories))")
+                    .font(.custom(FontType.light.rawValue, size: 16))
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: 300)
+                    .foregroundColor(Color.ui.text)
+            }
+            .padding(.horizontal)
+
+            if withCTA {
+                Button("Lakukan") {
+                    
+                }
+                .buttonStyle(PrimaryButtonStyle(isShort: true))
+                .padding(.horizontal)
+                .padding(.top, 10)
+            }
+        }
     }
 }
