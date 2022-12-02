234c3ee67f2241ee28ffe022cd08816957186ee9
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-12-02 09:20:47 +0700

    delete calendar ext

--- macro-team15/Extensions/View.swift
+++ macro-team15/Extensions/View.swift
@@ -51,12 +51,3 @@ extension Sequence where Element: Hashable {
     }
 }
 
-extension Calendar {
-    func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
-        let fromDate = startOfDay(for: from) // <1>
-        let toDate = startOfDay(for: to) // <2>
-        let numberOfDays = dateComponents([.month], from: fromDate, to: toDate) // <3>
-        
-        return numberOfDays.day!
-    }
-}
--- macro-team15/ViewModel/ViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewModel.swift
-//  macro-team15
-//
-//  Created by Aditya Cahyo on 28/11/22.
-//
-
-import Foundation
