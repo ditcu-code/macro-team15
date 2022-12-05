4f32b56ab679334eab671db9e5a63f98806d1198
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-06 01:41:37 +0700

    Milestone detail share

--- macro-team15/View/Milestone/MilestoneDetailView.swift
+++ macro-team15/View/Milestone/MilestoneDetailView.swift
@@ -77,6 +77,8 @@ struct MilestoneDetailViewV2: View {
     
     var milestone: Milestone
     var cdMilestone: BabyMilestone
+    
+    @State private var isChecked = false
     @State var refreshId: Int = 0
     @Environment(\.presentationMode) var presentationMode
     
@@ -147,10 +149,21 @@ struct MilestoneDetailViewV2: View {
                     .foregroundColor(Color.ui.primary)
                     .onTapGesture {
                         cdMilestone.isChecked.toggle()
-                        refreshId += 1
                         PersistenceController.shared.save()
+                        
+                        withAnimation {
+                            refreshId += 1
+                        }
+                        
+                        isChecked = cdMilestone.isChecked
                     }
             }
         }.id(refreshId)
+        
+        
+        .sheet(isPresented: $isChecked) {
+            ProgressShareView(title: milestone.title)
+                .presentationDetents([.height(600)])
+        }
     }
 }
