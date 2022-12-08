149a7cc7eddcd8e4401367fffb4dca21fbbdb40f
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-08 09:00:03 +0700

    Fixed onboarding layout

--- macro-team15/View/Screen/Onboarding/OnboardingView.swift
+++ macro-team15/View/Screen/Onboarding/OnboardingView.swift
@@ -11,59 +11,60 @@ struct OnboardingView: View {
     @StateObject var viewModel = OnboardingViewModel()
     
     var body: some View {
-        ZStack(alignment: .top) {
-            ZStack(alignment: .top) {
-                HStack {
-                    if viewModel.step > 1 {
-                        Button {
-                            viewModel.prevStep()
-                        } label: {
-                            Label("Kembali", systemImage: "chevron.left").labelStyle(.titleAndIcon)
-                        }
-                    }
-                    Spacer()
-                    if viewModel.step == 3 {
-                        Button("Lewati") {
-                            viewModel.finalStep()
-                        }
+        VStack {
+            HStack {
+                if viewModel.step > 1 {
+                    Button {
+                        viewModel.prevStep()
+                    } label: {
+                        Label("Kembali", systemImage: "chevron.left").labelStyle(.titleAndIcon)
                     }
                 }
-                .foregroundColor(Color.ui.primary)
-                .padding()
-                .offset(y: 30)
-            }.zIndex(1)
+
+                Spacer()
+                
+                if viewModel.step == 3 {
+                    Button("Lewati") {
+                        viewModel.finalStep()
+                    }
+                }
+            }
+            .foregroundColor(Color.ui.primary)
+            .padding()
             
-            ZStack(alignment: .bottom) {
-                BackgroundView()
-                
-                VStack {
-                    if (viewModel.step != 3) {
-                        TuntunIllustration(step: $viewModel.step)
-                            .padding(.bottom, 30)
-                    }
-                    switch viewModel.step {
-                    case 0:
-                        GreetingOnboarding(viewModel: viewModel)
-                        
-                    case 1:
-                        NameOnboarding(viewModel: viewModel)
-                        
-                    case 2:
-                        DateOnboarding(viewModel: viewModel)
-                        
-                    case 3:
-                        ProfilePictureOnboarding(viewModel: viewModel)
-                        
-                    default:
-                        GreetingOnboarding(viewModel: viewModel)
-                    }
+            Spacer()
+            
+            Group {
+                if (viewModel.step != 3) {
+                    TuntunIllustration(step: $viewModel.step)
+                        .padding(.bottom, 30)
                 }
-                .padding()
-                .offset(y: -UIScreen.main.bounds.height / 5)
                 
-            }.animation(.default, value: viewModel.step)
+                switch viewModel.step {
+                case 0:
+                    GreetingOnboarding(viewModel: viewModel)
+                    
+                case 1:
+                    NameOnboarding(viewModel: viewModel)
+                    
+                case 2:
+                    DateOnboarding(viewModel: viewModel)
+                    
+                case 3:
+                    ProfilePictureOnboarding(viewModel: viewModel)
+                    
+                default:
+                    GreetingOnboarding(viewModel: viewModel)
+                }
+            }
+            .padding()
+            
+            Spacer()
         }
-        
+        .animation(.default, value: viewModel.step)
+        .background {
+            BackgroundView()
+        }
     }
 }
 
